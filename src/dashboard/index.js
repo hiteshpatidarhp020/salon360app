f67308fd742f7722ff4bb93463d4164328ef3142
Hitesh <hiteshpatidarhp020>
2023-09-18 19:31:45 +0530

    branch for testing dashboard

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,6 +1,8 @@
 import style from "./dashboard.module.css";
 const Dashboard = () => {
-  return <div className={style.dashboardmain}>dashboard</div>;
+  return (
+    <div className={style.dashboardmain}>dashboard Test branch dashboard</div>
+  );
 };
 
 export default Dashboard;
